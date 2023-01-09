6fb84ba8df06929e7fdfa7eded73c39ae7b195a2
vamsilearner <vkp.in@yahoo.com>
2023-01-09 15:50:08 +0530

    Factwise Assessment design

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Coll from "./ant-coll/coll";
+import "./App.scss";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Coll />
+      </div>
+    </>
+  );
+}
+
+export default App;
